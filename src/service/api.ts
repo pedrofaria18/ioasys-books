cf70b1f0ac27bfc301c41cf7072220cd1de0eeca
pedrofaria18 <64284346+pedrofaria18@users.noreply.github.com>
2022-03-30 23:35:40 -0300

    refactor: :recycle: Tirando código comentado

--- src/service/api.ts
+++ src/service/api.ts
@@ -15,30 +15,6 @@ api.interceptors.request.use((config) => {
   return config
 })
 
-// api.interceptors.response.use(
-//   (response) => response,
-
-//   async (error) => {
-//     if (error.response.status === 401) {
-//       const refreshTokenStorage = localStorage.getItem('@Ioasys:RefreshToken')
-//       const { headers } = await api.post('/auth/refresh-token', {
-//         refreshToken: refreshTokenStorage,
-//       })
-//       if (headers) {
-//         const token = headers.authorization
-//         const refreshToken = headers['refresh-token']
-
-//         localStorage.setItem('@Ioasys:Token', token)
-//         localStorage.setItem('@Ioasys:RefreshToken', refreshToken)
-//         api.defaults.headers.common.Authorization = `Bearer ${token}`
-//       }
-
-//       return axios(error.config)
-//     }
-//     return Promise.reject(error)
-//   }
-// )
-
 api.interceptors.response.use(
   (response) => {
     return response
